35d8eb09ce9f3435445fa0ef62d33f3e7291c929
Fernando Ibarra <Fernandooibarra@gmail.com>
2025-05-20 19:55:02 -0700

    first commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,31 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+    type RouteConfig,
+    index,
+    layout,
+    route,
+    prefix,
+} from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index('routes/home.tsx'),
+
+    ...prefix('/auth', [
+        layout('layouts/auth-layout.tsx', [
+            route('login', 'routes/auth/login-page.tsx'),
+            route('register', 'routes/auth/register-page.tsx'),
+            route('testing', 'routes/auth/testing-page.tsx'),
+            route('testing-args/:id/:name/:age', 'routes/auth/testing-args-page.tsx'),
+
+            route('logout', 'auth/actions/logout.action.ts'),
+        ]),
+    ]),
+
+    ...prefix('/chat', [
+        layout('layouts/chat-layout.tsx', [
+            index('routes/chat/no-chat-selected-page.tsx'),
+            route('client/:id', 'routes/chat/client-chat-page.tsx'),
+        ]),
+    ]),
+
+    route('products/:name', 'routes/product.tsx'),
+] satisfies RouteConfig;
